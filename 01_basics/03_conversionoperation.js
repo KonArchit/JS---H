c43e6b0899915a1968087b05319ce49d4ecbaf57
Archit Agarwal <129601857+KonArchit@users.noreply.github.com>
2024-02-08 12:48:51 +0000

    Color Scheme Switcher

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -23,5 +23,21 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let s = 33
 
 let stringNumber = String(s)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// Operations
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " Archit"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(+true);
+console.log(+"");
+
